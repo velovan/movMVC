3d7d1d5f095b15590867981d2e26e7ccf426a7c9
velovan <velovan@users.noreply.github.com>
2022-03-14 09:27:50 +0200

    Update MovieServiceImpl.java

--- Mov_ss/src/main/java/com/jos/service/impl/MovieServiceImpl.java
+++ Mov_ss/src/main/java/com/jos/service/impl/MovieServiceImpl.java
@@ -16,27 +16,26 @@ public class MovieServiceImpl implements MovieService {
 	
 	@Transactional
 	public void add(Movie movie) {
-		movieDao.add(movie);
+	     movieDao.add(movie);
 	}
 
 	@Transactional
 	public void edit(Movie movie) {
-		movieDao.edit(movie);
+	     movieDao.edit(movie);
 	}
 
 	@Transactional
 	public void delete(int id) {
-		movieDao.delete(id);
+	     movieDao.delete(id);
 	}
 
 	@Transactional
 	public Movie getMovie(int id) {
-		return movieDao.getMovie(id);
+	     return movieDao.getMovie(id);
 	}
 
 	@Transactional
 	public List getAllMovie() {
-		return movieDao.getAllMovie();
+	     return movieDao.getAllMovie();
 	}
-
 }
